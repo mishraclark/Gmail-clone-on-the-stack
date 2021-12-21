3382152b27268d9a65fd9196590b91d19c247b9c
Mishra Clark <mishraclark@gmail.com>
2021-12-21 08:28:16 -0800

    added checkbox functionality

--- src/components/MailTable.vue
+++ src/components/MailTable.vue
@@ -7,47 +7,62 @@
           @click="selectScreen(archive)"
           :disabled="selectedScreen == 'archive'">Archived</button>
 </div>
+<h1>{{emailSelection.emails.size}} emails selected</h1>
   <table class="max-w-full m-auto border-collapse">
     <tbody>
       <tr v-for="email in unarchivedEmails"
           :key="email.id"
           :class="['cursor-pointer', email.read ? 'bg-gray-300' : 'bg-white']"
-          @click="openEmail(email)"
           >
         <td class="border-b border-t p-1 text-left">
-          <input class="hover:border-2 w-6 h-6 border align-middle px-10 relative rounded-sm bg-white cursor-pointer" type="checkbox" />
+          <input class="hover:border-2 w-6 h-6 border align-middle px-10 relative rounded-sm bg-white cursor-pointer" type="checkbox"  @click="emailSelection.toggle(email)"
+                 :selected="emailSelection.emails.has(email)" />
         </td>
-        <td class="border-b p-1 text-left">{{email.from}}</td>
-        <td class="border-b p-1 text-left">
+        <td class="border-b p-1 text-left" @click="openEmail(email)">{{email.from}}</td>
+        <td class="border-b p-1 text-left" @click="openEmail(email)">
           <p class="overflow-hidden max-h-5 px-5"><strong>{{email.subject}}</strong> - {{email.body}}</p>
         </td>
-        <td class="border-b p-1 text-left w-32">{{format(new Date(email.sentAt), 'MMM do yyyy')}}</td>
+        <td class="border-b p-1 text-left w-32" @click="openEmail(email)">{{format(new Date(email.sentAt), 'MMM do yyyy')}}</td>
         <td class="border-b p-1 text-left" ><button class="btn" @click="email.archived = true">Archive</button></td>
       </tr>
     </tbody>
   </table>
    <ModalView v-if="openedEmail" @closeModal="openedEmail = null">
-    <MailView :email="openedEmail" />
+    <MailView :email="openedEmail" @changeEmail="changeEmail" />
   </ModalView>
 </template>
 
 <script>
   import { format } from 'date-fns';
   import axios from 'axios';
-  import { ref } from 'vue';
+  import { ref, reactive } from 'vue';
   import MailView from './MailView.vue';
   import ModalView from './ModalView.vue';
+  import { useEmailSelection } from '../composables/use-email-selection';
 
-  export default {
+   export default {
     async setup(){
       let {data: emails} = await axios.get('http://localhost:3000/emails')
+      let selected = reactive(new Set())
+      let emailSelection = {
+        emails: selected,
+        toggle(email) {
+          if(selected.has(email)) {
+            selected.delete(email)
+          } else {
+            selected.add(email)
+          }
+          console.log(selected)
+        }
+      }
       return {
+        emailSelection: useEmailSelection(),
         format,
         emails: ref(emails),
         openedEmail: ref(null)
       }
     },
-     components: {
+    components: {
       MailView,
       ModalView
     },
@@ -63,17 +78,28 @@
     },
     methods: {
       openEmail(email) {
-        email.read = true
-        this.updateEmail(email)
         this.openedEmail = email
-      },
-      readEmail(email) {
-        email.read = true
-        this.updateEmail(email)
+        if(email) {
+          email.read = true
+          this.updateEmail(email)
+        }
       },
       archiveEmail(email) {
         email.archived = true
         this.updateEmail(email)
+      },
+      changeEmail({toggleRead, toggleArchive, save, closeModal, changeIndex}) {
+        let email = this.openedEmail
+        if(toggleRead) { email.read = !email.read }
+        if(toggleArchive) { email.archived = !email.archived }
+        if(save) { this.updateEmail(email) }
+        if(closeModal) { this.openedEmail = null }
+        if(changeIndex) {
+          let emails = this.unarchivedEmails
+          let currentIndex = emails.indexOf(this.openedEmail)
+          let newEmail = emails[currentIndex + changeIndex]
+          this.openEmail(newEmail)
+        }
       },
       updateEmail(email) {
         axios.put(`http://localhost:3000/emails/${email.id}`, email)
@@ -82,6 +108,5 @@
   }
 </script>
 
-<style>
-
+<style scoped>
 </style>
