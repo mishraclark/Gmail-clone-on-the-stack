8a4d7b972a7e4bfa32ee5483d0f498463578c4ab
Mishra Clark <mishraclark@gmail.com>
2021-12-28 14:52:58 -0800

    fixed README

--- src/stores/emailStore.ts
+++ src/stores/emailStore.ts
@@ -1,6 +1,4 @@
-import { Email } from './emailStore';
 import { defineStore } from 'pinia'
-import { reactive, ref } from 'vue'
 import axios from 'axios'
 
 // useStore could be anything like useUser, useCart
@@ -16,15 +14,34 @@ export interface Email {
 }
 export type RootState = {
   emails: Email[];
+  selectedScreen: string;
 };
 
 export const emailStore = defineStore('main', {
     state: () => ({
-        emails: []
+        emails: [],
+        selectedScreen: 'inbox'
        } as RootState),
+
+      getters: {
+        sortedEmails(): Email[] {
+          return this.emails.sort((e1, e2) => {
+            return e1.sentAt < e2.sentAt ? 1 : -1
+          })
+        },
+        filteredEmails() {
+          if(this.selectedScreen == 'inbox') {
+            return this.sortedEmails.filter(e => !e.archived)
+          } else {
+            return this.sortedEmails.filter(e => e.archived)
+          }
+      }
+      }, 
+
       actions: {
         async callEmails() {
             this.emails = (await axios.get('http://localhost:3000/emails')).data
-          }
-        }
+          },
+          
+      }
 })
